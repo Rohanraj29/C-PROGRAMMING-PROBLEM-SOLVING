7a4dd77b3e99841d6ba31afe744105d27957f07c
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-03-29 21:51:47 +0530

    Add files via upload

---
+++ value check.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+int m,n;
+printf ("Enter the number");
+scanf ("%d", & m);
+if (m==0)
+printf ("The value of n is 1");
+else 
+if (m>0)
+printf ("Value.of n is 1");
+else 
+printf ("Value is -1");
+    return 0;
+}

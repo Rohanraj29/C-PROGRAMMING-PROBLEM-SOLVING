ea8733c3697db3dd62ce13e1fd7388f22e5d3d17
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-04-02 22:12:32 +0530

    Add files via upload

---
+++ length of string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <string.h>
+int main ()
+{
+char a[100]= "My selfs Rohan Raj";
+int Length ;
+printf ("Word is %s\n", a);
+Length=strlen(a);
+printf ("Length of word is %d\n", Length);
+return 0;
+}

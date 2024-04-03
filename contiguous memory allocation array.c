efa8a3177dac1b36216cb504767b7c810e801874
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-04-03 22:25:22 +0530

    Add files via upload

---
+++ contiguous memory allocation array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+//char a[5]={'R','O','H','A','N'};
+int a[5]={2,4,6,8,1};
+printf ("%d\n",&a[0]);
+printf ("%d\n",&a[1]);
+printf ("%d\n",&a[2]);
+printf ("%d\n",&a[3]);
+printf ("%d\n",&a[4]);
+
+    return 0;
+}

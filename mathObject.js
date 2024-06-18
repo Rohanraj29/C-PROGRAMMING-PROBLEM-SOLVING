13d3a139f624cc5079312dfe55acf88aa3e5c0ad
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-06-18 19:58:15 +0530

    Add files via upload

---
+++ mathObject.js
@@ -0,0 +1,14 @@
+//IN THIS WE CAN SHOW THE VARIOUS TYPES OF MATH OBJECTS.
+//WHAT IS MATH OBJECT?
+//A MATH OBJECT IS A PREDEFINED OBJECT IN JAVASCRIPT THAT HAVE SOME PROPERTIES AND METHODS. ITS PROPERTIES CAN ALSO BE PREDEFINED.
+//WE CAN THAT WE CAN ACCESS AN OBJECT PROPERTIES BY USING OBJECT NAME.PROPERTIES NAME.
+//THERE ARE VARIOUS TYPE OF PRE-DEFINED PROPERTIES.THESE ARE AS FOLLOWS:-
+console.log(Math.pi) //here math is an object and pi is the properties of that math object.
+console.log(Math.E)
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.8))
+console.log(Math.floor(4.2))
+console.log(Math.max(2,4,6,8))
+console.log(Math.min(2,4,6,8))
+console.log(Math.pow(2,4))
+console.log(Math.sqrt(8))
